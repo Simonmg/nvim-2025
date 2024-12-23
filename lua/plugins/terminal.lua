99c1119b616bb47350579a9604c91b8d108cb923
Simón Malavé <semg20@gmail.com>
2024-12-22 22:28:02 -0400

    wip: terminal feature

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,16 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    size = function(term)
+      if term.direction == "horizontal" then
+        return 15
+      elseif term.direction == "vertical" then
+        return vim.o.columns * 0.2
+      end
+    end,
+    open_mapping = [[<c-t>]],
+    direction = "vertical",
+    on_open = function(term) end,
+  },
+}
