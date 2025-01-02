5107264ea50cc36cd5de7d0c87d608ac33c94679
SimonmgClaro <smalave@claroinsurance.com>
2025-01-02 09:02:02 -0400

    chore: testing float terminal dev

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,15 +1,22 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = function(_, opts)
-    opts.size = function(term)
-      if term.direction == "horizontal" then
-        return 15
-      elseif term.direction == "vertical" then
-        return vim.o.columns * 0.2
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = function(_, opts)
+      opts.size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.2
+        end
       end
-    end
-    opts.open_mapping = [[<c-t>]]
-    opts.direction = "vertical"
-  end,
+      opts.open_mapping = [[<c-t>]]
+      opts.direction = "float"
+      opts.float_opts = {
+        border = "curved",
+      }
+
+      opts.cmd = "tmux"
+    end,
+  },
 }
