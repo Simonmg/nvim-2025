3cce9f7dde8b523c3bc8423887325894277a8be4
Simon Malave <semg20@gmail.com>
2025-11-28 21:56:53 -0400

    fix: terminal toggle

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,6 +2,9 @@ return {
   {
     "akinsho/toggleterm.nvim",
     version = "*",
+    keys = {
+      { "<c-t>", "<cmd>ToggleTerm<cr>", desc = "Toggle Terminal" },
+    },
     opts = function(_, opts)
       opts.size = function(term)
         if term.direction == "horizontal" then
@@ -10,11 +13,18 @@ return {
           return vim.o.columns * 0.2
         end
       end
-      opts.open_mapping = [[<c-t>]]
+      opts.open_mapping = false
       opts.direction = "float"
       opts.float_opts = {
         border = "curved",
       }
+      opts.on_open = function(term)
+        vim.api.nvim_buf_set_keymap(term.bufnr, "t", "<c-t>", "<C-\\><C-n><cmd>ToggleTerm<cr>", {
+          noremap = true,
+          silent = true,
+          desc = "Toggle terminal",
+        })
+      end
     end,
   },
 }
